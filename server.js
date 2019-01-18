ab66c896c9a1ce3b7dac66ce4dd8f647e187e8c6
agraham <agraham0925@gmail.com>
2019-01-17 20:56:34 -0600

    console logs people data

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3000;
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const fetch = require('node-fetch');
+
+// const api = 'https://swapi.co/api/person';
+
+
 app.get('/', (request, response) => {
 	response.send('hi im a server')
 })
 
+app.get('/:id', (req, res) => {
+	const index = req.params.id
 
-app.listen(3000, () => {
+	const url = "https://swapi.co/api/people/" + index
+
+	const getData = async url => {
+  		try {
+   			const response = await fetch(url);
+   			const json = await response.json();
+   			console.log(json);
+  		} catch (error) {
+    	console.log(error);
+  	}
+};
+
+getData(url);
+
+	// fetch("https://swapi.co/api/person/" + index)
+	// 	.then(res => res.json())
+ //    	.then(json => console.log(json));
+
+	// res.json(res);
+})
+
+
+app.listen(PORT, () => {
 	console.log('server is listening on Port 3000');
 })
